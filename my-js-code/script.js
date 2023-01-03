95120d684e2b842f25dc3e94979ad9328974d3bd
Yrysgul Ergesh <yrysgulergesh@gmail.com>
2023-01-04 00:21:30 +0600

    let and const

--- my-js-code/script.js
+++ my-js-code/script.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+// console.log('Hello World!');
+// console.error('This is an error');
+// console.warn('This is a warning');
+
+// let, const
+
+let age = 30;
+age = 31;
+
+const score = 10;
+
+console.log(age);
+console.log(score);
